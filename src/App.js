4f7c660cf711ce308d8b7d9c49ddaac478cbf0fe
Samson Tmg <stmg202023@gmail.com>
2023-05-23 15:42:00 +0545

    Learn axios/post/url/send data/not submit name/network/FETCH/XHR/Headers/payload/ etc.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Title from "./components/Title";
 // import FirstReq from "./examples/1-first-request";
-import SecondReq from "./examples/2-headers";
+// import SecondReq from "./examples/2-headers";
+import PostReq from "./examples/3-post-request";
 
 function App() {
   return (
     <main>
       <Title />
       {/* <FirstReq /> */}
-      <SecondReq />
+      {/* <SecondReq /> */}
+      <PostReq />
     </main>
   );
 }
