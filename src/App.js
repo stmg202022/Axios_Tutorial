fa1c428884a5e8fa6558a16fe532deecd759f849
Samson Tmg <stmg202023@gmail.com>
2023-05-23 14:47:17 +0545

    axios/get/url/headers/Accept/application/json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Title from "./components/Title";
-import Setup from "./examples/1-first-request";
+// import FirstReq from "./examples/1-first-request";
+import SecondReq from "./examples/2-headers";
 
 function App() {
   return (
     <main>
       <Title />
-      <Setup />
+      {/* <FirstReq /> */}
+      <SecondReq />
     </main>
   );
 }
